9d63d37df7b34fc0a7757d363a4e826ca287f658
Shawn He <shawnhe1991@gmail.com>
2023-02-27 11:42:31 -0500

    complete code

---
+++ assertEqual.js
@@ -0,0 +1,18 @@
+/**
+ * @param {boolean compare values} testing strings or numbers to see if they match
+ * @return {assert pass or fail}
+ */
+
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if ((actual === expected)) {
+    console.log(`✅✅✅ assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 assertion failed: ${actual} != ${expected}`);
+  }
+
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
