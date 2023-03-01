02b973184487113f62c8b9f58023ffbc4ee07af2
Shawn He <shawnhe1991@gmail.com>
2023-03-01 15:36:52 -0500

    complete code

---
+++ without.js
@@ -0,0 +1,33 @@
+/**
+ * The below function creates an array excluding all given values
+ * 
+ * @param {array} array the array to inspect
+ * @param {value} value to remove from string 
+ * @returns {array} Returns the new array without filtered value
+ */
+
+//create function
+const without = function(arr, val){
+  //create array
+  let arr1 = [];
+  let counter = 0;
+
+  //write loop to check if there's the value equals the toher value. write 2 for loops.
+  for (i = 0; i < arr.length; i++) {
+    counter = 0;
+    for (x = 0; x < val.length; x++) {
+  // boolean conditional to see if array index value =  val  value
+    if (arr[i] === val[x]) {
+      counter ++;
+    }
+    }
+    if (counter === 0) {
+      arr1.push(arr[i]);
+    } 
+  }
+  return arr1;
+}
+
+
+console.log(without([1, 2, 3], [1])) // => [2, 3]
+console.log(without(["1", "2", "3"], [1, 2, "3"])) // => ["1", "2"]
