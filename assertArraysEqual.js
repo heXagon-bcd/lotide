7cf0687559ef787bb5033cdb84107bbb75699d23
Shawn He <shawnhe1991@gmail.com>
2023-03-17 13:54:30 -0400

    update to moduralize

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,15 +1,19 @@
-const eqArrays = function(str1, str2) {
-  if (JSON.stringify(str1) == JSON.stringify(str2)) {
-    console.log("true");
-  } else {
-    console.log("false");
-  }
-}
+// const eqArrays = function(str1, str2) {
+//   if (JSON.stringify(str1) == JSON.stringify(str2)) {
+//     console.log("true");
+//   } else {
+//     console.log("false");
+//   }
+// }
 
-const eqArrays = function(str1, str2) {
+const assertEqual = require('../assertEqual')
+
+const eqArraysAssert = function(str1, str2) {
   if (JSON.stringify(str1) === JSON.stringify(str2)) {
     return true;
   } else {
     return false;
   }
 }
+
+module.exports = eqArraysAssert;
