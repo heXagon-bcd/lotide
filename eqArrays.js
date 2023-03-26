570f90afdeaac8db6474948542c9ff2baf2a82ee
Shawn He <shawnhe1991@gmail.com>
2023-03-26 16:13:50 -0400

    lint code

--- eqArrays.js
+++ eqArrays.js
@@ -1,19 +1,13 @@
 /**
+ * this method can compare arrays against each other to see if values within arrays are the same.
  * @param {boolean compare values} testing strings or numbers to see if they match
  * @return {assert pass or fail}
+ * TEST CODE
+ * console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
+ * assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
  */
 
-// // FUNCTION IMPLEMENTATION
-// const assertEqual = function(actual, expected) {
-//   if ((actual === expected)) {
-//     console.log(`✅✅✅ assertion passed: ${actual} === ${expected}`);
-//   } else {
-//     console.log(`🛑🛑🛑 assertion failed: ${actual} != ${expected}`);
-//   }
-
-// };
-
-const assertEqual = require('./assertEqual')
+const assertEqual = require('./assertEqual');
 
 const eqArrays = function(str1, str2) {
   if (JSON.stringify(str1) === JSON.stringify(str2)) {
@@ -21,10 +15,5 @@ const eqArrays = function(str1, str2) {
   } else {
     return false;
   }
-}
+};
 module.exports = {eqArrays, assertEqual};
-
-
-// console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
-
-// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
