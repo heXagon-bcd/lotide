7ee621480375e19e5968275daae4d06c6f22e264
Shawn He <shawnhe1991@gmail.com>
2023-03-26 16:10:44 -0400

    update documnetation

--- head.js
+++ head.js
@@ -1,7 +1,7 @@
 /**
  * This method determines the first value of an array
- * @param {boolean compare values} testing strings or numbers to see if they match
- * @return {assert pass or fail}
+ * @param {array} input array
+ * @return {value} first index position of an array
  * TEST CODE
  * head([5,6,7] => will result in 5
  */
