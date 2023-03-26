4e21a998f7b49a1b12944f74e84460e23bcce3d1
Shawn He <shawnhe1991@gmail.com>
2023-03-26 18:36:04 -0400

    lint complete

--- countLetters.js
+++ countLetters.js
@@ -1,34 +1,23 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if ((actual === expected)) {
-    console.log(`✅✅✅ assertion passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 assertion failed: ${actual} != ${expected}`);
-  }
-
-};
-
 /**
  * @param {string} the function takes in a setence as a string and return a count  of the letters int he sequence
  * Step 1 Create Function
  * Step 2 Create an empty Object
  * Step 3 Loop trough each string character
+ * EXAMPLE
+ * console.log(countLetters('hello shawn'));
  */
+
+const assertEqual = require("./assertEqual");
 const countLetters = function(sentence) {
-let sentenceCount = {};
-  for(letter of sentence) {
-    //const element = sentence[letter];
+  let sentenceCount = {};
+  for (let letter of sentence) {
     sentenceCount[letter] = (sentenceCount[letter] || 0) + 1;
   }
-   console.log("expected Object", sentenceCount[' '])
-  // for(space of Object.keys(sentenceCount)) {
-  //   if((' ' in sentenceCount) === true) {
-  //     // delete sentenceCount[space];
-  //     console.log(sentenceCount[space]);
-  //   }
-  //}
-    delete sentenceCount[' '];
-return sentenceCount
-}
+  console.log("expected Object", sentenceCount[' ']);
+  delete sentenceCount[' '];
+  return sentenceCount;
+};
 
-console.log(countLetters('hello shawn'));
+
+
+module.exports = {countLetters};
