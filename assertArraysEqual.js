152e1499cacd156dc937463ab8dac3fca4101ec8
Shawn He <shawnhe1991@gmail.com>
2023-03-26 16:18:03 -0400

    lint file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,12 +1,11 @@
-// const eqArrays = function(str1, str2) {
-//   if (JSON.stringify(str1) == JSON.stringify(str2)) {
-//     console.log("true");
-//   } else {
-//     console.log("false");
-//   }
-// }
-
-const assertEqual = require('../assertEqual')
+/**
+ * this method can compare arrays against each other to see if values within arrays are the same.
+ * @param {boolean compare values} testing strings or numbers to see if they match
+ * @return {assert pass or fail}
+ * TEST CODE
+ * console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
+ * assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+ */
 
 const eqArraysAssert = function(str1, str2) {
   if (JSON.stringify(str1) === JSON.stringify(str2)) {
@@ -14,6 +13,6 @@ const eqArraysAssert = function(str1, str2) {
   } else {
     return false;
   }
-}
+};
 
 module.exports = eqArraysAssert;
