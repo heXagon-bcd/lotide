84c80377b18e22b1089b7cf2a69338336c1115dd
Shawn He <shawnhe1991@gmail.com>
2023-03-26 19:03:50 -0400

    lint

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,39 +1,29 @@
-//const eqObjects = require ('./eqObjects');
+/**
+ * This method will detmerine if objects have the same values
+ * @param {object}input first object that you want to compare
+ * @param {object}input second object that you want to compare
+ * @return {assert pass or fail}
+ * TEST
+ * const shirtObject = { color: "red", size: "medium" };
+ * const anotherShirtObject= { size: "medium", color: "red" };
+ * eqObjects(shirtObject , anotherShirtObject); // => true
+ */
+
+// Returns true if both objects have identical keys with identical values.
+// Otherwise you get back a big fat false!
+
+
 //Eq objects
 const eqObjects = function(object1, object2) {
   let arr1 = Object.keys(object1);
-  console.log(arr1)
   let arr2 = Object.keys(object2);
-  for(ky in object1) {
-    console.log(object1[ky], object2[ky])
-    if (object1[ky] === object2[ky] && arr1.length === arr2.length ) {
-      return true
+  for (let ky in object1) {
+    if (object1[ky] === object2[ky] && arr1.length === arr2.length) {
+      return true;
     } else {
-      return false
+      return false;
     }
   }
 };
 
-// FUNCTION IMPLEMENTATION
-const assertObjectsEqual = function(actual, expected) {
- 
-  const inspect = require('util').inspect; // <= add this line
-  if (eqObjects(actual, expected)) {
-    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)} `)
-  } else (
-    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)} `)
-  )
-};
-
-const shirtObject = { color: "red", size: "medium" };
-const anotherShirtObject= { size: "medium", color: "red" };
-
-assertObjectsEqual(shirtObject, anotherShirtObject);
-
-// assertObjectsEqual(shirtObject , anotherShirtObject);
-
-//console.log(`Example label: ${inspect(actual)}`);
-
-
-//✅✅✅ Assertion Passed: [object Object] === [object Object]
-//🛑🛑🛑 Assertion Failed: [object Object] !== [object Object]
+module.exports = eqObjects;
