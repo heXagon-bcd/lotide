b024dbfca63ee1baa3f008b8beb677164d2d0ebe
Shawn He <shawnhe1991@gmail.com>
2023-03-29 10:58:47 -0400

    complete map func

--- map.js
+++ map.js
@@ -1,30 +1,12 @@
-
-
 /**
  * Map function
- * 
  * REQUIREMENTS
  * @param {array} array to clone
  * @param {callback function} function to return value
+ * test function
+ * console.log(results1);
+ * eqArrays(results1, ['g', 'c', 't', 'm', 't'])===> true
  */
-const words = ["ground", "control", "to", "major", "tom"];
-
-const map = function (array, callback) {
-  arr1 = [];
-  for(i = 0; i < array.length; i++) {
-    arr1.push[array[i]]
-  }
-}
-//
-const eqArrays = function(str1, str2) {
-  if (JSON.stringify(str1) == JSON.stringify(str2)) {
-    console.log("true");
-  } else {
-    console.log("false");
-  }
-}
-
-
 const words = ["ground", "control", "to", "major", "tom"];
 
 const map = function(array, callback) {
@@ -33,8 +15,6 @@ const map = function(array, callback) {
     results.push(callback(item));
   }
   return results;
-}
-
+};
 const results1 = map(words, word => word[0]);
 console.log(results1);
-eqArrays(results1, ['g', 'c', 't', 'm', 't'])
