6fddcfd4823eb4afb43a9193cf09386be2610313
Shawn He <shawnhe1991@gmail.com>
2023-03-29 11:02:56 -0400

    complete takeUnitl

--- takeUntil.js
+++ takeUntil.js
@@ -3,36 +3,23 @@
  * It should keep going until the callback/predicate returns a truthy value.
  * @param {array}
  * @param {callback}
+ * TEST
+ * const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+ * const results1 = takeUntil(data1, x => x < 0); // function(x) {return x < 0}
+ * console.log(results1);
+ * console.log('---');
+ * -----
+ * const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+ * const results2 = takeUntil(data2, x => x === ',');
+ * console.log(results2);
  */
-
-
 const takeUntil = function(array, callback) {
   let arr1 = [];
-  for (i = 0; i < array.length ; i++) {
+  for (let i = 0; i < array.length; i++) {
     if (callback(array[i])) {//truthy if array[i] < 0
       return arr1;//
     }
     arr1.push(array[i]);//if true, push array[i] to arr1
   }
   return arr1;//  return if false
-}
-
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results1 = takeUntil(data1, x => x < 0); // function(x) {return x < 0}
-console.log(results1);
-
-console.log('---');
-
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const results2 = takeUntil(data2, x => x === ',');
-console.log(results2);
-
-// [1,2,3].forEach(function (num) {
-//   console.log('num: ', num);
-// });
-
-// const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-// const results1 = data1.forEach(function(x) {
-//   console.log(x < 0);
-// });
-// console.log(results1);
+};
